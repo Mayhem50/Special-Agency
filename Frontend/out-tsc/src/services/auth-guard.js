d02b8a85ed6c69fb41c27b7a17c140214ff6c30f
Benjamin REGNIER <regnier.ben@gmail.com>
2021-03-04 12:10:11 +0100

    Add common styles

--- Frontend/out-tsc/src/services/auth-guard.js
+++
@@ -1,9 +0,0 @@
-import AuthorizationService from "./authorization";
-export class AuthGuard {
-    static navigate(context, commands, redirectPath) {
-        if (!AuthorizationService.instance().isAuthorized()) {
-            return commands.redirect(redirectPath !== null && redirectPath !== void 0 ? redirectPath : '/');
-        }
-    }
-}
-//# sourceMappingURL=auth-guard.js.map
--- Frontend/out-tsc/src/styles/common.js
+++
@@ -1,7 +0,0 @@
-import { css } from 'lit-element';
-export default css `
-  .error {
-    color: var(--error);
-  }
-`;
-//# sourceMappingURL=common.js.map
